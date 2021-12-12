413576e7a73f8f5f92c6994521cda35e0b6ca8aa
Fredrik Rothamel <fredrik.rothamel@alelion.com>
2021-12-12 18:29:33 +0100

    11

---
+++ 11/dumbo2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import fileinput
+
+data = [[int(c) for c in line.strip()] for line in fileinput.input()]
+
+def check(row, col):
+    if 0<=row<len(data) and 0<=col<len(data[0]):
+        data[row][col] += 1
+        if data[row][col] == 10:
+            check(row-1, col-1)
+            check(row-1, col)
+            check(row-1, col+1)
+            check(row,   col-1)
+            check(row,   col+1)
+            check(row+1, col-1)
+            check(row+1, col)
+            check(row+1, col+1)
+
+def pprint(d):
+    for r in data:
+        print(''.join([str(c) for c in r]))
+
+tot = len(data) * len(data[0])
+for step in range(1,1000):
+    count = 0
+    for row in range(len(data)):
+        for col in range(len(data[0])):
+            check(row, col)
+    for row in data:
+        for col,c in enumerate(row):
+            if c>9:
+                count += 1
+                row[col] = 0
+    if count == tot:
+        print(step)
+        break
